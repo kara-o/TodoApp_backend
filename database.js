2f2a65633baa5b85f36fe7b7a0fccfc777ef3327
kara-o <44376276+kara-o@users.noreply.github.com>
2020-02-28 22:30:42 -0800

    established conn to postgres, node proxy server to react frontend

--- database.js
+++ database.js
@@ -1,3 +1,53 @@
 require('dotenv').config();
 
 const { Pool } = require('pg');
+const isProduction = process.env.NODE_ENV === 'production';
+
+// const connectionString = `postgres://<user>:<password>@<host>:<port>/<database>?<query>`
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction
+});
+
+const getTodos = (req, res) => {
+  pool.query('SELECT * FROM todos ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const getTodoById = (req, res) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM todos WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const createTodo = (req, res) => {
+  const { text } = request.body;
+
+  pool.query(
+    'INSERT INTO todos (text) VALUES ($1)',
+    [text],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`User added with ID: ${result.insertId}`);
+    }
+  );
+};
+
+module.exports = {
+  getTodos,
+  getTodoById,
+  createTodo
+};
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
-
-const { pool } = require('./database');
-
+const bodyParser = require('body-parser');
 const app = express();
+const port = process.env.PORT || 5000;
+const db = require('./database');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.get('/', (req, res) => {
-  res.send({ message: 'endpoint working' });
+  res.json({ info: 'Node, Express, Postgres API' });
 });
 
 //start server
 app.listen(port, () => {
-  console.log(`Server running at: http://localhost:${port}/`);
+  console.log(`Server listening`);
+});
+
+//for each endpoint, set HTTP request method, endpoint URL path, and relevant function
+app.get('/todos', db.getTodos);
+app.get('/todos/:id', db.getTodoById);
+app.post('/todos', db.createTodo);
+
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
